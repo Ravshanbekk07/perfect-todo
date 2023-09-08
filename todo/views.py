d134ce5e198df3df511daaff4bff4f4b14dc8be8
Ravshanbekk07 <ramazonof07@gmail.com>
2023-09-08 16:51:28 +0500

    views3

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Category,Task
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
@@ -22,4 +22,80 @@ class UserRegistrationView(APIView):
             token=Token.objects.get_or_create(user=user)
             return Response({'token':token.key,'message':'user registered successfully'},status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+
+    def post(self,request):
+        token, created = Token.objects.get_or_create(user=request.user)
+        return Response({'token': token.key, 'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+
+class CategoryListView(APIView):
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+
+    def get(self,request):
+        categories=Category.objects.filter(user=request.user)
+        serializer=CategorySerializer(categories,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self,request):
+            request.data['user']=request.user.id
+            serializer=CategorySerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
+class TaskListView(APIView):
+    def get(self,request):
+        authentication_classes=[TokenAuthentication]
+        permission_classes=[IsAuthenticated]
+
+        params=request.query_params
+
+        category=params.get('category',False)
+        priority=params.get('priority',False)
+
+        if category and priority:
+            tasks =Task.objects.filter(user=request.user,category__name__icontains=category,priority__icontains=priority)
+        else:
+            tasks=Task.objects.filter(user=request.user)
+
+        serializer=TaskSerializer(tasks,many=True)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+
+    def post(self,request):
+        request.data['user']=request.user.id
+        serializer=TaskCreateSerializer(data=request.data,context={'request': request})
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailView(APIView):
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+
+    def get(self,request,pk:int):
+        task = get_object_or_404(Task,id=pk,user=request.user)
+
+        serializer = TaskSerializer(task)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def put(self,request,pk:int):
+        task=get_object_or_404(Task,id=pk,user=request.data)
+        serializer=TaskSerializer(task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self,request,pk):
+        task=get_object_or_404(Task,id=pk,user=request.user)
+        task.delete()
+        return Response({"message": "Task has been deleted successfully."})
