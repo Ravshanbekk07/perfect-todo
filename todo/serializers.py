7327ad04056057e7d2b6df183faa8ea5692111d0
Ravshanbekk07 <ramazonof07@gmail.com>
2023-09-08 12:07:29 +0500

    serializers2

--- todo/serializers.py
+++ todo/serializers.py
@@ -55,3 +55,49 @@ class TaskCreateSerializer(serializers.ModelSerializer):
         }
     
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields='__all__'
+
+    def create(self,validated_data):
+        if Category.objects.filter(user=validated_data['user'],name=validated_data['name']).exists():
+            raise serializers.ValidationError('this already exists')
+        category =Category.objects.create(
+            user=validated_data['user'],
+            name=validated_data['name'],
+        )
+        return category
+    
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'name': instance.name
+        }
+    
+class UserRegisterSerializer(serializers.ModelSerializer):
+    
+    
+    username=serializers.CharField(max_length=150)
+    email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
+
+    def validate_username(self,value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('This username is already taken')
+        return value
+    def validate_email(self,value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('This username is already taken')
+        return value
+    def validate_password(self,value):
+        validate_password(value)
+        return value
+    
+    def create(self,validated_data):
+        user=User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+        return user
